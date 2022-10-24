e468f01b2e55a08f5a84bbcf40383271e56ce614
Francisco Alex <franciscoalex.bio@gmail.com>
2022-10-24 17:53:15 -0300

    primeiro commit

---
+++ aula 01/cometarios.py
@@ -0,0 +1,11 @@
+# Aula sobre comentários
+
+# Este é um comentário de um única linha
+
+print('Olá mundo!')
+
+#print('Estou aprendendo Python')
+
+'''
+ Este é um comentário de múltiplas linhas / docstrings
+'''
---
+++ aula 2/print.py
@@ -0,0 +1,20 @@
+
+# end='' -> evita quebra de linha
+# sep='' -> separa por algo especial
+
+print(12345)
+
+# print(type(12334))
+
+print('Francisco', 'Alex') # O que tem dentro da função print são argumentos
+print('Francisco', 'Alex ', sep='-', end='')
+print('João e Maria', 'gosta de café', sep='*')
+
+# --------------------------------------------------------------------------
+
+# mostrar CPF
+
+print('241', '', sep='.', end='')
+print('231', '', sep='.', end='')
+print('321', '', sep='-', end='')
+print('43')
