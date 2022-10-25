6be8e4ea59306a9792bf66c14c55b284ff0294f7
Francisco Alex <franciscoalex.bio@gmail.com>
2022-10-25 09:21:25 -0300

    aula 3 e 4

---
+++ aula 3/strings_aspas.py
@@ -0,0 +1,12 @@
+# Strings e aspas
+# Python possui tipagem dinâmica
+
+
+print('Isto é uma string')
+print('Alguma coisa')
+print('3245')
+
+print('"texto dentro de aspas"')
+print('texto \'qualquer\'') # utilizando caracter de escape
+print('adicionando uma quebra \nde linha')
+print(r'nada é para ser executado com o "r"')
---
+++ aula 4/tipos de dados.py
@@ -0,0 +1,19 @@
+# tipos de dados primitivos
+# str, float, int, bool
+
+print('4 = 4: ', 4 == 4)
+print('4 = "4": ', 4 == '4')
+
+print(20 * '-')
+
+print('alex: ', type('alex'))
+print(123,':', type(123))
+print('True: ', type(True))
+print(32.2,':', type(32.2))
+
+print(20 * '-')
+
+# typecasting
+
+print(type('Alex'))
+print(type(bool('Alex')))
