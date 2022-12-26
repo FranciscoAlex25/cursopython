d617ccb227cbba821f9dd113a1a7f28e0e09eac9
FrranciscoAlex25 <franciscoalex.flp@gmail.com>
2022-12-26 10:39:21 -0300

    aula 23

--- aula 23/listas - desafio.py
+++ aula 23/listas - desafio.py
@@ -1,4 +1,13 @@
+'''
+remove(value) - append() - pop(indice) - clear() - del() - insert() -
+'''
+
 nomes = ['alex', 'karol', 'julio', 'matheus']
+
+a, *_ = nomes
 
 for i in range(len(nomes)):
     print(f'{i} - {nomes[i]}')
+
+print(a)
+print(_)
