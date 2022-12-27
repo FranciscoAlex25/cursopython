5488f10a44f9d3dc71ab7868511f7ba9d72a5d06
FrranciscoAlex25 <franciscoalex.flp@gmail.com>
2022-12-27 16:01:43 -0300

    segunda parte do desafio

--- projeto final.py/segunda parte/script.py
+++ projeto final.py/segunda parte/script.py
@@ -1,5 +1,5 @@
 cpf = '462.945.480-20'
-soma = contador = valor = prime = 0 
+soma = contador = valor = primeiro_digito = 0 
 
 # listas necessárias
 lista_com_valores_regressiva = [10, 9, 8, 7, 6, 5, 4, 3, 2]
@@ -28,4 +28,31 @@ resultado = (soma * 10) % 11
 # obtendo o dígito 
 primeiro_digito = resultado if resultado <= 9 else 0
 
-print(primeiro_digito)
+# ----------------------------------------------------------------------------------------
+soma = 0 
+resultado = 0 
+contador = 0
+
+# listas necessárias
+lista_com_valores_regressiva = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+lista_com_valores_multiplicados = []
+lista_oficial.append(primeiro_digito)
+
+# dicionando os valores multiplicados à lista
+for i in range(2, 12):
+    valor = int(lista_com_valores_regressiva[contador]) * int(lista_oficial[contador])
+    lista_com_valores_multiplicados.append(valor)
+    contador += 1
+
+# somando os valores da lista
+for i in lista_com_valores_multiplicados:
+    soma += int(i)
+
+# operações necessárias
+resultado = (soma * 10) % 11
+
+# obtendo o dígito 
+segundo_digito = resultado if resultado <= 9 else 0
+
+print(f'Primeiro dígto: {primeiro_digito} ')
+print(f'Segundo dígito: {segundo_digito}')
