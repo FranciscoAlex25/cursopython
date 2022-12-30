fcae9c1d957d22acf23ad09a1499e91bd0b05373
FrranciscoAlex25 <franciscoalex.flp@gmail.com>
2022-12-30 19:07:26 -0300

    aula 14

---
+++ módulo 2/aula 14/set.py
@@ -0,0 +1,23 @@
+
+valores = {1, 3, 3, 4, 5, 111, 2, 3, 1, 1}
+nome = {'Francisco', 'Francisco', 'Alex'}
+
+# for i in nome:
+#     if 'F' in i:
+#         print('ok')
+
+
+nome.add('Ana')
+nome.update(('José',))
+nome.discard('Ana')
+valores.clear()
+
+print(nome)
+
+'''
+| - união 
+& - intersecão 
+- - contém apenas 
+^ - não contém em ambos
+'''
+
