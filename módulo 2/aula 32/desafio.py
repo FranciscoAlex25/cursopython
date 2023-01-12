cfc11854204eed81f1c4667b3d324f56155d0051
FrranciscoAlex25 <franciscoalex.flp@gmail.com>
2023-01-12 17:17:19 -0300

    aula 32

--- módulo 2/aula 32/desafio.py
+++ módulo 2/aula 32/desafio.py
@@ -1,9 +1,10 @@
  #Exercício - Lista de tarefas com desfazer e refazer
 
-import os
+import os, json 
 
 lista_de_tarefas = []
 lista_desfazer = []
+caminho = os.path.dirname(__file__ + '/')
 
 
 def desfazer_tarefa():
@@ -12,9 +13,15 @@ def desfazer_tarefa():
     if len(lista_de_tarefas) >= 1:
         ultima_tarefa = lista_de_tarefas.pop()
         lista_desfazer.append(ultima_tarefa)
+        print()
         return lista_de_tarefas
-
+    print()
     return 'NÃO HÁ O QUE DESFAZER!'
+
+
+def cadastrar_tarefa(opcao):
+    lista_de_tarefas.append(opcao)
+    os.system('clear')
 
 
 def refazer_tarefa():
@@ -23,8 +30,9 @@ def refazer_tarefa():
     if len(lista_desfazer) >= 1:
         ultima_tarefa_desfeita = lista_desfazer.pop()
         lista_de_tarefas.append(ultima_tarefa_desfeita)
+        print()
         return lista_de_tarefas
-
+    print()
     return 'NÃO HÁ O QUE REFAZER!'
 
 
@@ -37,23 +45,26 @@ def listar_tarefa():
         print(lista_de_tarefas)
 
 
+def conver_json():
+    with open(caminho + 'arquivo.json', 'w') as arquivo:
+        json.dump(lista_de_tarefas, arquivo)
+
+
 def exibir():
     print('Comando: Listar - Desfazer - Refazer')
     opcao = input('Digite uma tarefa ou comando: ').title()
- 
-    if opcao != 'Listar' and opcao != 'Desfazer' and opcao != 'Refazer':
-        lista_de_tarefas.append(opcao)
-        os.system('clear')
-    elif opcao == 'Listar':
-        listar_tarefa()
-        print()
-    elif opcao == 'Desfazer':
-        print(desfazer_tarefa())
-        print()
-    else:
-        print(refazer_tarefa())
-        print()
-   
+    
+    try:
+        comandos = {
+            'Listar': lambda: listar_tarefa(),
+            'Desfazer': lambda: print(desfazer_tarefa()),
+            'Refazer': lambda: print(refazer_tarefa())
+        }
+        comando = comandos.get(opcao)()
+    except:
+        cadastrar_tarefa(opcao)
+
+    conver_json()
     return exibir()
 
 
