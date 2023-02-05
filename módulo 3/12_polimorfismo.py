73cb14670b40f32ca1ea2606b889c3bc9a8df79a
FrranciscoAlex25 <franciscoalex.flp@gmail.com>
2023-02-05 10:26:37 -0300

    criando button, entry e label

--- módulo 3/12_polimorfismo.py
+++ módulo 3/12_polimorfismo.py
@@ -11,7 +11,7 @@ class Notificacao(ABC):
 
 class NotificacaoEmail(Notificacao):
     def enviar_mensagem(self):
-        print('EMAIL:', self.mensagem)
+        print('EMAIL: ', self.mensagem)
 
 
 notificacao = NotificacaoEmail('testando')
